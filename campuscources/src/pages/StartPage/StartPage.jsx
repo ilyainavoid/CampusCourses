166f555e86c3c7411793388cb4a6fed753879777
ilyainavoid <thirdilno@gmail.com>
2024-03-24 23:09:58 +0700

    feat: add start page

---
+++ campuscources/src/pages/StartPage/StartPage.jsx
@@ -0,0 +1,9 @@
+import {Flex} from "antd";
+
+export default function StartPage() {
+    return (
+        <Flex style={{alignItems: 'center', justifyContent: 'center', padding: '20px'}}>
+            <p style={{fontSize: '40px'}}>Добро пожаловать в систему кампусных курсов</p>
+        </Flex>
+    )
+}
